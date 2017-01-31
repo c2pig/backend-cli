f3e31d6745d24c130db105d93c0b45835d835162
c2pig <c2pigisme@yahoo.com>
2017-02-01 01:24:15 +0800

    add be-cli options supports

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 #!/usr/bin/env node
+'use strict';
 var yeoman = require('yeoman-environment');
+var path = require('path');
+var parseArgs = require('minimist')(process.argv.slice(2));
+var modulePath = require.resolve('generator-gdp-backend');
 var env = yeoman.createEnv();
+var cmds = { 'list': [], 'gen': ['config-path', 'output', 'props'], 'configure': ['path'] };
+var gens = {};
+env.register(modulePath,'main');
 
-env.register(require.resolve('generator-gdp-backend'), 'be:cli');
+let registerGenerator = (name) => {
+  gens[name] = env.register(path.dirname(modulePath) + `/${name}/index.js`, name);
+}
+Object.keys(cmds).forEach((name) => {
+  registerGenerator(name);
+});
 
-// The #lookup() method will search the user computer for installed generators.
-// The search if done from the current working directory.
 env.lookup(function () {
-  env.run('be:cli', {'skip-install': false}, function (err) {
-    console.log(err);
-  });
+  for(let cmd in cmds) {
+    if(parseArgs._.indexOf(cmd) > -1) {
+      let options = {};
+      cmds[cmd].forEach((opt) => {
+        options[opt] = parseArgs[opt] || '';
+      });
+      env.run(cmd, options, function (err) {
+        if(err) {
+          console.log(err);
+        }
+      });
+      break;
+    }
+  }
 });
